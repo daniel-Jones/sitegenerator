3e5d066e3376f9c5da397f3c537260abda025367
daniel-Jones <daniel@danieljon.es>
2018-01-28 17:14:03 +1030

    added kawaii template and added the generation to the live script

--- live.sh
+++ live.sh
@@ -1,5 +1,5 @@
 python3 generatesite.py
 python3 generatesite.py templates/template_dark.txt dark
 python3 generatesite.py templates/template_nocss.txt nocss
-
+python3 generatesite.py templates/template_kawaii.txt kawaii
 cp -r output/* /var/www
