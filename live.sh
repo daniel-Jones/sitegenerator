4543af5bd8b19a0e2c1bb67dcbcf6a4e7cd160c9
Daniel Jones <daniel@danieljon.es>
2018-02-10 00:31:00 -0500

    changed live script to edit permissions appropriately

--- live.sh
+++ live.sh
@@ -3,3 +3,6 @@ python3 generatesite.py templates/template_dark.txt dark
 python3 generatesite.py templates/template_nocss.txt nocss
 python3 generatesite.py templates/template_kawaii.txt kawaii
 cp -r output/* /var/www
+chmod -R +r /var/www/dark
+chmod -R +r /var/www/kawaii
+chmod -R +r /var/www/nocss
