38d450a43510394aa632b3ec2d379983e90d55b2
Daniel Jones <daniel@danieljon.es>
2018-02-12 20:33:27 -0500

    added hacky fix for /blog permissions

--- live.sh
+++ live.sh
@@ -3,6 +3,7 @@ python3 generatesite.py templates/template_dark.txt dark
 python3 generatesite.py templates/template_nocss.txt nocss
 python3 generatesite.py templates/template_kawaii.txt kawaii
 cp -r output/* /var/www
+chmod -R +r /var/www/blog
 chmod -R +r /var/www/dark
 chmod -R +r /var/www/kawaii
 chmod -R +r /var/www/nocss
