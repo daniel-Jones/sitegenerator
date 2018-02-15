fd89ee0a8050fbd301e362ec80878f25852bb049
daniel-Jones <daniel@danieljon.es>
2018-02-15 20:43:38 +1030

    last push did not have live.sh commited

--- live.sh
+++ live.sh
@@ -2,8 +2,10 @@ python3 generatesite.py
 python3 generatesite.py templates/template_dark.txt dark
 python3 generatesite.py templates/template_nocss.txt nocss
 python3 generatesite.py templates/template_kawaii.txt kawaii
+python3 generatesite.py templates/template_light.txt light
 cp -r output/* /var/www
 chmod -R +r /var/www/blog
 chmod -R +r /var/www/dark
 chmod -R +r /var/www/kawaii
 chmod -R +r /var/www/nocss
+chmod -R +r /var/www/light
