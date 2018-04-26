fdb7babca5abc100655d384c68e5229eb81ae7b9
Daniel Jones <daniel@danieljon.es>
2018-04-26 01:12:55 -0400

    added sync script to copy media directories

---
+++ sync.sh
@@ -0,0 +1,25 @@
+# sync all the media directories, this only has to be run when new media is added
+
+# root media directory
+rsync -r /var/www/media /var/www/kawaii/
+rsync -r /var/www/media /var/www/dark/
+rsync -r /var/www/media /var/www/light/
+rsync -r /var/www/media /var/www/nocss/
+
+# blog media directory
+rsync -r /var/www/blog/media /var/www/kawaii/blog/
+rsync -r /var/www/blog/media /var/www/dark/blog/
+rsync -r /var/www/blog/media /var/www/light/blog/
+rsync -r /var/www/blog/media /var/www/nocss/blog/
+
+# waifu media directory
+rsync -r /var/www/waifus/media /var/www/kawaii/waifus/
+rsync -r /var/www/waifus/media /var/www/dark/waifus/
+rsync -r /var/www/waifus/media /var/www/light/waifus/
+rsync -r /var/www/waifus/media /var/www/nocss/waifus/
+
+# opinions media directory
+rsync -r /var/www/opinions/media /var/www/kawaii/opinions/
+rsync -r /var/www/opinions/media /var/www/dark/opinions/
+rsync -r /var/www/opinions/media /var/www/light/opinions/
+rsync -r /var/www/opinions/media /var/www/nocss/opinions/
