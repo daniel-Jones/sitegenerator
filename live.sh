d365ef07be8d5e3066211983bea59371a55ee087
daniel-Jones <daniel@danieljon.es>
2018-06-11 09:42:53 +0930

    changed default theme to light, added solarized template and link

--- live.sh
+++ live.sh
@@ -3,9 +3,11 @@ python3 generatesite.py templates/template_dark.txt dark
 python3 generatesite.py templates/template_nocss.txt nocss
 python3 generatesite.py templates/template_kawaii.txt kawaii
 python3 generatesite.py templates/template_light.txt light
+python3 generatesite.py templates/template_solarized.txt solarized 
 cp -r output/* /var/www
 chmod -R +r /var/www/blog
 chmod -R +r /var/www/dark
 chmod -R +r /var/www/kawaii
 chmod -R +r /var/www/nocss
 chmod -R +r /var/www/light
+chmod -R +r /var/www/solarized
--- sync.sh
+++ sync.sh
@@ -5,21 +5,25 @@ rsync -r /var/www/media /var/www/kawaii/
 rsync -r /var/www/media /var/www/dark/
 rsync -r /var/www/media /var/www/light/
 rsync -r /var/www/media /var/www/nocss/
+rsync -r /var/www/media /var/www/solarized/
 
 # blog media directory
 rsync -r /var/www/blog/media /var/www/kawaii/blog/
 rsync -r /var/www/blog/media /var/www/dark/blog/
 rsync -r /var/www/blog/media /var/www/light/blog/
 rsync -r /var/www/blog/media /var/www/nocss/blog/
+rsync -r /var/www/blog/media /var/www/solarized/blog/
 
 # waifu media directory
 rsync -r /var/www/waifus/media /var/www/kawaii/waifus/
 rsync -r /var/www/waifus/media /var/www/dark/waifus/
 rsync -r /var/www/waifus/media /var/www/light/waifus/
 rsync -r /var/www/waifus/media /var/www/nocss/waifus/
+rsync -r /var/www/waifus/media /var/www/solarized/waifus/
 
 # opinions media directory
 rsync -r /var/www/opinions/media /var/www/kawaii/opinions/
 rsync -r /var/www/opinions/media /var/www/dark/opinions/
 rsync -r /var/www/opinions/media /var/www/light/opinions/
 rsync -r /var/www/opinions/media /var/www/nocss/opinions/
+rsync -r /var/www/opinions/media /var/www/solarized/opinions/
