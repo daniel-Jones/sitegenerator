430618c0b91f6ace59ff0bce0569430a6e426384
Daniel Jones <admin@danieljon.es>
2020-01-06 16:56:28 +1030

    added script to push site live

--- live.sh
+++ live.sh
@@ -1,14 +1,4 @@
-python3 generatesite.py
-python3 generatesite.py templates/template_dark.txt dark
-python3 generatesite.py templates/template_nocss.txt nocss
-python3 generatesite.py templates/template_kawaii.txt kawaii
-python3 generatesite.py templates/template_light.txt light
-python3 generatesite.py templates/template_solarized.txt solarized 
+./generate
 cp -r output/* /var/www/html
 chmod -R +r /var/www/html/blog
 chmod -R +r /var/www/html/posts
-chmod -R +r /var/www/html/dark
-chmod -R +r /var/www/html/kawaii
-chmod -R +r /var/www/html/nocss
-chmod -R +r /var/www/html/light
-chmod -R +r /var/www/html/solarized
